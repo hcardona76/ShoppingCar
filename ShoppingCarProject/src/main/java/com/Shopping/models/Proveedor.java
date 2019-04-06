efd220d0f5a9adc5a75aee8eabf1211fbad0150a
SantiagoMuneraMarin <48155485+SantiagoMuneraMarin@users.noreply.github.com>
2019-04-06 08:55:46 -0500

    Santiago Múnera

--- ShoppingCarProject/src/main/java/com/Shopping/models/Proveedor.java
+++ ShoppingCarProject/src/main/java/com/Shopping/models/Proveedor.java
@@ -4,14 +4,14 @@ public class Proveedor {
 
 	
 
-		private final int ID;
-		private final String Razon_Social;
-		private final int RUT;
-		private final int Telefono_Contacto;
-		private final String Direccion; 
-		private final String Horario_Disponibilidad;
-		private final String Contacto;
-		private final String Productos;
+		private int ID;
+		private String Razon_Social;
+		private int RUT;
+		private int Telefono_Contacto;
+		private String Direccion; 
+		private String Horario_Disponibilidad;
+		private String Contacto;
+		private String Productos;
 		
 		
 		
@@ -26,40 +26,102 @@ public class Proveedor {
 			this.Productos = Productos;
 		}
 
-				
+
+
 		public int getID() {
 			return ID;
 		}
-		
+
+
+
+		public void setID(int iD) {
+			ID = iD;
+		}
+
+
+
 		public String getRazon_Social() {
 			return Razon_Social;
 		}
-		
+
+
+
+		public void setRazon_Social(String razon_Social) {
+			Razon_Social = razon_Social;
+		}
+
+
+
 		public int getRUT() {
 			return RUT;
 		}
-		
+
+
+
+		public void setRUT(int rUT) {
+			RUT = rUT;
+		}
+
+
+
 		public int getTelefono_Contacto() {
 			return Telefono_Contacto;
 		}
-		
+
+
+
+		public void setTelefono_Contacto(int telefono_Contacto) {
+			Telefono_Contacto = telefono_Contacto;
+		}
+
+
+
 		public String getDireccion() {
 			return Direccion;
 		}
-		
+
+
+
+		public void setDireccion(String direccion) {
+			Direccion = direccion;
+		}
+
+
+
 		public String getHorario_Disponibilidad() {
 			return Horario_Disponibilidad;
 		}
-		
+
+
+
+		public void setHorario_Disponibilidad(String horario_Disponibilidad) {
+			Horario_Disponibilidad = horario_Disponibilidad;
+		}
+
+
+
 		public String getContacto() {
 			return Contacto;
 		}
-		
+
+
+
+		public void setContacto(String contacto) {
+			Contacto = contacto;
+		}
+
+
+
 		public String getProductos() {
 			return Productos;
 		}
 
-		
+
+
+		public void setProductos(String productos) {
+			Productos = productos;
+		}
+
 		
 		
 	}

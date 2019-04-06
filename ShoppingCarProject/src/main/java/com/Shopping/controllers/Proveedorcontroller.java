44b929df977c07581a77bdebd27dcc689a11e533
SantiagoMuneraMarin <48155485+SantiagoMuneraMarin@users.noreply.github.com>
2019-04-06 09:59:19 -0500

    s

--- ShoppingCarProject/src/main/java/com/Shopping/controllers/Proveedorcontroller.java
+++ ShoppingCarProject/src/main/java/com/Shopping/controllers/Proveedorcontroller.java
@@ -1,5 +1,13 @@
 package com.Shopping.controllers;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path="/pos")
+@ComponentScan(basePackages="com.Shopping")
+
 public class Proveedorcontroller {
 
 }

f584aa8845b1a336fb639acdcc97f5210d53ffb4
zeroqlst <48155362+zeroqlst@users.noreply.github.com>
2019-04-06 09:45:17 -0500

    Actualizacion

--- ShoppingCarProject/src/main/java/com/Shopping/models/Forma_pago.java
+++ ShoppingCarProject/src/main/java/com/Shopping/models/Forma_pago.java
@@ -1,24 +1,23 @@
 package com.Shopping.models;
 
-import javax.persistence.GenerationType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Forma_pago {
 
-
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private final int id;
 	private final int idCliente;
 	private final String Nombre;
 	private final String email;
 	private final int idReferenciaTiket;
-	
-	@Entity
+		
 	public Forma_pago(int Cliente,String Nombre,String email,int ticket) {
-		@id
-		@GenerateValue(strategy=GenerationType.AUTO)
+		
 		this.id=1;
 		this.idCliente=Cliente;
 		this.Nombre=Nombre;
@@ -54,5 +53,13 @@ public class Forma_pago {
 	public static boolean Efectivo() {
 		return false;
 	}
+
+	@Override
+	public String toString() {
+		return "Forma_pago [id=" + id + ", idCliente=" + idCliente + ", Nombre=" + Nombre + ", email=" + email
+				+ ", idReferenciaTiket=" + idReferenciaTiket + "]";
+	}
+	
+	
 	
 }

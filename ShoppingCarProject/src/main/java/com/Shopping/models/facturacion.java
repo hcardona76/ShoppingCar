2dcde8a0d84e2c826594b412b20c2d070104bc05
pablopatino <pablopatino97@hotmail.com>
2019-04-06 08:55:11 -0500

    Update facturacion.java

--- ShoppingCarProject/src/main/java/com/Shopping/models/facturacion.java
+++ ShoppingCarProject/src/main/java/com/Shopping/models/facturacion.java
@@ -1,28 +1,8 @@
 package com.Shopping.models;
 
 public class facturacion {
-<<<<<<< HEAD
 
-	
-	private long idfacturacion;
-	private long idCompra;
-	private long idCliente;
-	private long idProveedor;
-	private String productos; 
-	private long costoTotal; 
-	private String fecha;
-	
-	public Cliente(long cedula, String nombrecompleto, String telefono, String direccion, String celular, String tipo) {
-		this.cedula=cedula;
-		this.nombrecompleto=nombrecompleto;
-		this.telefono= telefono;
-		this.direccion = direccion;
-		this.celular = celular;
-		this.tipo = tipo;
-	}
-	
-	
-=======
+
 	private final int id;
 	private final int idCliente;
 	private final String Nombre;

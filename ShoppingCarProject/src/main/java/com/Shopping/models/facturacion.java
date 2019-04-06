78c8896f7c892268c81518e265f1732f5e659973
Practicas <Practicas@PC124.salas.local>
2019-04-06 08:47:50 -0500

    Update facturacion.java

--- ShoppingCarProject/src/main/java/com/Shopping/models/facturacion.java
+++ ShoppingCarProject/src/main/java/com/Shopping/models/facturacion.java
@@ -7,6 +7,15 @@ public class facturacion {
 	private final String email;
 	private final int idReferenciaTiket;
 	
+	
+	public facturacion(int Cliente,String Nombre,String email,int ticket) {
+		this.id=1;
+		this.idCliente=Cliente;
+		this.Nombre=Nombre;
+		this.email=email;
+		this.idReferenciaTiket=ticket;
+	}
+	
 	public int getId() {
 		return id;
 	}
@@ -24,17 +33,16 @@ public class facturacion {
 	}
 	
 	public static int Tarjeta_Credito() {
-		
-		
+		return 0;		
 	}
 	
 	
 	public static int PSE() {
-		
+		return 0;
 	}
 	
 	public static boolean Efectivo() {
-		
+		return false;
 	}
 	
 }

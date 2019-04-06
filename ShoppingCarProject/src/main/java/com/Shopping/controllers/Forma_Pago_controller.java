bce808fd76342709a600c2c5ce01f4c8fa173e38
zeroqlst <48155362+zeroqlst@users.noreply.github.com>
2019-04-06 09:58:29 -0500

    Actualizar

--- ShoppingCarProject/src/main/java/com/Shopping/controllers/Forma_Pago_controller.java
+++ ShoppingCarProject/src/main/java/com/Shopping/controllers/Forma_Pago_controller.java
@@ -1,10 +1,12 @@
 package com.Shopping.controllers;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path="/Forma_Pago")
+@ComponentScan(basePackages="com.Shopping")
 public class Forma_Pago_controller {
 
 }
--- ShoppingCarProject/src/main/java/com/Shopping/controllers/RegistroUsuario_controller.java
+++ ShoppingCarProject/src/main/java/com/Shopping/controllers/RegistroUsuario_controller.java
@@ -1,10 +1,12 @@
 package com.Shopping.controllers;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path="/RegistroUsuario")
+@ComponentScan(basePackages="com.Shopping")
 public class RegistroUsuario_controller {
 
 }

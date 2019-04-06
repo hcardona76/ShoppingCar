db8339b15c6c190644f07c880ad6bd11ce7bfb72
zeroqlst <48155362+zeroqlst@users.noreply.github.com>
2019-04-06 09:54:15 -0500

    Actualizacion

--- ShoppingCarProject/src/main/java/com/Shopping/controllers/Forma_Pago_controller.java
+++ ShoppingCarProject/src/main/java/com/Shopping/controllers/Forma_Pago_controller.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(path="Forma_Pago")
+@RequestMapping(path="/Forma_Pago")
 public class Forma_Pago_controller {
 
 }

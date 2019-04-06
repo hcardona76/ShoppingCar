542d9f48fd5d369dbf8ff82b06da367b1213fde5
pablopatino <pablopatino97@hotmail.com>
2019-04-06 09:59:49 -0500

    g

--- ShoppingCarProject/src/main/java/com/Shopping/controllers/pedidocontroller.java
+++ ShoppingCarProject/src/main/java/com/Shopping/controllers/pedidocontroller.java
@@ -1,5 +1,22 @@
 package com.Shopping.controllers;
 
-public class pedidocontroller {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+
+@Controller
+@ComponentScan(basePackages = "com.Shopping")
+@RequestMapping(path="/pos")
+
+public class pedidocontroller {
+	
+	
+	
+	
+	
+	
+	
 }

be72aeb7e1c290a6cc89b365a87ead02efb913e2
zeroqlst <48155362+zeroqlst@users.noreply.github.com>
2019-04-13 08:08:38 -0500

    act

--- ShoppingCarProject/src/main/java/com/Shopping/controllers/Forma_Pago_controller.java
+++ ShoppingCarProject/src/main/java/com/Shopping/controllers/Forma_Pago_controller.java
@@ -1,22 +1,21 @@
 package com.Shopping.controllers;
 
-import java.util.List;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.Shopping.repository.Forma_Pago_repository;
 
 @Controller
 @RequestMapping(path="/Forma_Pago")
 @ComponentScan(basePackages="com.Shopping")
 public class Forma_Pago_controller {
-	private Forma_Pago_repository FormaPago;
+	
+	private Forma_Pago_repository FormaPagoRepository;
 
-/*	public @ResponseBody Iterable<Forma_Pago_repository> getAllFormaPago() {
-		return FormaPago.getAll;
+	public @ResponseBody Forma_Pago_repository getAllFormaPago() {
+		return FormaPagoRepository;
 	}
-*/
+
 }
--- ShoppingCarProject/src/main/java/com/Shopping/repository/Forma_Pago_repository.java
+++ ShoppingCarProject/src/main/java/com/Shopping/repository/Forma_Pago_repository.java
@@ -2,10 +2,9 @@ package com.Shopping.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import com.Shopping.models.Forma_pago;
-import java.util.*;
+import java.util.List;;
 
 public interface Forma_Pago_repository extends CrudRepository<Forma_pago,Integer>  {
 	
-	List<Forma_pago> getListByName(String name);
 	
 }

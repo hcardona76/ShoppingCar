96e722cf80db06bac158e1bdf9b873ad626e6152
pablopatino <pablopatino97@hotmail.com>
2019-04-13 08:16:27 -0500

    b

--- ShoppingCarProject/src/main/java/com/Shopping/repository/RegistroUsuario_repository.java
+++ ShoppingCarProject/src/main/java/com/Shopping/repository/RegistroUsuario_repository.java
@@ -2,7 +2,8 @@ package com.Shopping.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import com.Shopping.controllers.RegistroUsuario_controller;
+import com.Shopping.models.RegistroUsuario;
 
-public interface RegistroUsuario_repository extends CrudRepository<RegistroUsuario_controller,Integer>  {
+public interface RegistroUsuario_repository extends CrudRepository<RegistroUsuario,Integer>  {
 
 }
